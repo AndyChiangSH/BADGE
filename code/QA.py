404fc860b9243c3526ee840afb04b1e1bbc8c975
AndyChiangSH <andy10801@gmail.com>
2024-04-20 16:36:52 +0800

    Add csv

--- code/QA.py
+++ code/QA.py
@@ -9,10 +9,13 @@ if __name__ == '__main__':
     player_B = "Ng Ka Long Angus"
     competition = "YONEX Thailand Open 2021 Finals"
     
-    output = f"{competition}: {player_A} v.s. {player_B}"
+    QA_output = f"{competition}: {player_A} v.s. {player_B}"
+    csv_output = f"{competition}: {player_A} v.s. {player_B}"
     
     # Get list of filenames in the folder
     filenames = os.listdir(f"data/{game}/")
+    os.makedirs(f"csv/{game}/", exist_ok=True)
+    os.makedirs(f"QA/{game}/", exist_ok=True)
     # print("filenames:", filenames)
     set = 1
     for filename in filenames:
@@ -28,16 +31,23 @@ if __name__ == '__main__':
         print("> Translate...")
         translated_df = translate(preprocessed_df)
         # print("translated_df:", translated_df)
+        translated_df.to_csv(f"csv/{game}/{filename}", index=False)
+        
+        with open(f"csv/{game}/{filename}", "r") as f:
+            csv = f.read()
+            csv_output += f"\n\nSet {set}:\n{csv}"
         
         print("> Generate answer...")
         answers = generate_answer(translated_df, player_A, player_B)
-        print("answers:", answers)
+        # print("answers:", answers)
         
-        output += f"\n\nSet {set}:{answers}"
+        QA_output += f"\n\nSet {set}:{answers}"
         set += 1
 
     # Output
     print("Output...")
-    os.makedirs(f"QA/{game}/")
     with open(f"QA/{game}/QA.txt", "w") as f:
-        f.write(output)
+        f.write(QA_output)
+        
+    with open(f"csv/{game}/csv.txt", "w") as f:
+        f.write(csv_output)
