6e13b87eaa26ab464abfab5a2372a44d14faefec
AndyChiangSH <andy10801@gmail.com>
2024-04-24 14:00:14 +0800

    Human evaluation

---
+++ code/evaluation/human.py
@@ -0,0 +1,42 @@
+import os
+from openai import OpenAI
+
+
+if __name__ == '__main__':
+    model = "gpt-4-turbo-2024-04-09"
+    metrics = ["coherence", "consistency", "fluency", "excitement"]
+    
+    # Get list of filenames in the folder
+    games = os.listdir(f"report/")
+    
+    for game in games:
+        print("==================================")
+        print("game:", game)
+        
+        with open(f"report/{game}/human/human.txt", "r") as f:
+            data = f.read()
+            
+        for metric in metrics:
+            print("metric:", metric)
+            
+            with open(f"prompt/evaluation/{metric}.txt", "r") as f:
+                prompt = f.read()
+                
+            prompt = prompt.replace("{Badminton Report}", data)
+            print("prompt:\n", prompt)
+
+            client = OpenAI()
+
+            completion = client.chat.completions.create(
+                model=model,
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+
+            evaluation = completion.choices[0].message.content
+            print("evaluation:\n", evaluation)
+
+            os.makedirs(f"evaluation/{game}/human/", exist_ok=True)
+            with open(f"evaluation/{game}/human/human.txt", "w") as f:
+                f.write(evaluation)
