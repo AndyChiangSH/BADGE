d711dece39ba27a4a48b63f12e308400772d444e
AndyChiangSH <andy10801@gmail.com>
2024-07-08 15:52:12 +0800

    Update csv+QA.py

--- code/data/csv+QA.py
+++ code/data/csv+QA.py
@@ -4,23 +4,23 @@ from pipeline import preprocess, translate, generate_answer
 
 
 if __name__ == '__main__':
-    game = "Viktor_Axelsen_Ng_Ka_Long_Angus_YONEX_Thailand_Open_2021_Finals"
-    player_A = "Viktor Axelsen"
-    player_B = "Ng Ka Long Angus"
-    competition = "YONEX Thailand Open 2021 Finals"
+    game = "Kento_MOMOTA_CHOU_Tien_Chen_Denmark_Open_2018_Finals"
+    player_A = "Kento MOMOTA"
+    player_B = "CHOU Tien Chen"
+    competition = "Denmark Open 2018 Finals"
     
     QA_output = f"{competition}: {player_A} v.s. {player_B}"
     csv_output = f"{competition}: {player_A} v.s. {player_B}"
     
     # Get list of filenames in the folder
-    filenames = os.listdir(f"data/{game}/")
+    num_set = len(os.listdir(f"data/{game}/"))
     os.makedirs(f"csv/{game}/", exist_ok=True)
     os.makedirs(f"QA/{game}/", exist_ok=True)
-    # print("filenames:", filenames)
-    set = 1
-    for filename in filenames:
+    for set in range(1, num_set+1):
+        filename = f"set{set}"
+        
         print(f"> Read file {filename}...")
-        data = pandas.read_csv(f"data/{game}/{filename}")
+        data = pandas.read_csv(f"data/{game}/{filename}.csv")
         input_df = pandas.DataFrame(data)
         # print("input_df:", input_df)
 
@@ -31,18 +31,23 @@ if __name__ == '__main__':
         print("> Translate...")
         translated_df = translate(preprocessed_df)
         # print("translated_df:", translated_df)
-        translated_df.to_csv(f"csv/{game}/{filename}", index=False)
         
-        with open(f"csv/{game}/{filename}", "r") as f:
+        translated_df_copy = translated_df.copy()
+        translated_df_copy.replace(
+            {'win_point_player': {"A": player_A, "B": player_B}}, inplace=True)
+        
+        translated_df_copy.to_csv(f"csv/{game}/{filename}.csv", index=False)
+        
+        with open(f"csv/{game}/{filename}.csv", "r") as f:
             csv = f.read()
-            csv_output += f"\n\nSet {set}:\n{csv}"
+        
+        csv_output += f"\n\nSet {set}:\n{csv}"
         
         print("> Generate answer...")
         answers = generate_answer(translated_df, player_A, player_B)
         # print("answers:", answers)
         
         QA_output += f"\n\nSet {set}:{answers}"
-        set += 1
 
     # Output
     print("Output...")
