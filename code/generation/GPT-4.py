6d7911553ec063ebfc3162fbffb56c5a6b27b6fe
AndyChiangSH <andy10801@gmail.com>
2024-07-16 17:05:50 +0800

    Report generated by GPT-4

--- code/generation/GPT-4.py
+++ code/generation/GPT-4.py
@@ -4,14 +4,15 @@ from openai import OpenAI
 
 if __name__ == '__main__':
     model = "gpt-4-turbo-2024-04-09"
-    ICLs = ["csv+zero-shot", "csv+one-shot", "csv+few-shot", "csv+CoT", "QA+zero-shot", "QA+one-shot", "QA+few-shot", "QA+CoT"]
+    ICLs = ["CSV+zero-shot", "CSV+one-shot", "CSV+few-shot", "CSV+CoT",
+            "QA+zero-shot", "QA+one-shot", "QA+few-shot", "QA+CoT"]
 
-    for ICL in ICLs:    
+    for ICL in ICLs:
         print("> ICL:", ICL)
-        
-        with open(f"prompt/ICL/{ICL}.txt", "r") as f:
+
+        with open(f"prompt/generation/{ICL}.txt", "r") as f:
             prompt = f.read()
-            
+
         data_type = ICL.split("+")[0]
         print("> data_type:", data_type)
 
@@ -21,12 +22,12 @@ if __name__ == '__main__':
             print(">> game:", game)
             with open(f"{data_type}/{game}/{data_type}.txt", "r") as f:
                 data = f.read()
-                
-            if data_type == "csv":
-                prompt_data = prompt.replace("{csv table}", data)
+
+            if data_type == "CSV":
+                prompt_data = prompt.replace("{CSV}", data)
             elif data_type == "QA":
-                prompt_data = prompt.replace("{Q&A}", data)
-                
+                prompt_data = prompt.replace("{QA}", data)
+
             print(">> prompt_data:\n", prompt_data)
 
             client = OpenAI()
@@ -41,8 +42,8 @@ if __name__ == '__main__':
             report = completion.choices[0].message.content
             print(">> report:\n", report)
 
-            os.makedirs(f"report/{game}/GPT-4/", exist_ok=True)
-            with open(f"report/{game}/GPT-4/{ICL}.txt", "w") as f:
+            os.makedirs(f"generation/{game}/GPT-4/", exist_ok=True)
+            with open(f"generation/{game}/GPT-4/{ICL}.txt", "w") as f:
                 f.write(report)
-                
+
             print("===============================================")
